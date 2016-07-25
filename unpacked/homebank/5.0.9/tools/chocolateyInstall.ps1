514456fb585531bf1a6533702c47a173830c723f
Hadrien Dussuel <dbkblk@gmail.com>
2016-07-25 07:52:34 +0200

    homebank updated to 5.0.9

---
+++ unpacked/homebank/5.0.9/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+$packageName = 'homebank'
+$installerType = 'EXE'
+$url = 'http://homebank.free.fr/public/HomeBank-5.0.9-setup.exe'
+$silentArgs = '/silent'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
