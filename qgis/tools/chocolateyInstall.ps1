0c1d1d8ca36f69710bd294dd6ea56fac030e42d1
Hadrien Dussuel <dbkblk@gmail.com>
2016-08-12 22:56:23 +0200

    Preliminary support for qgis autoupdate

---
+++ qgis/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'qgis' 
+$installerType = 'exe' 
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrl64}}'
+$checksum = '{{Checksum}}'
+$checksum64 = '{{Checksum64}}'
+$checkumType = 'sha256'
+$silentArgs = '/S' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -checksum $checksum -checksumType $checkumType -checksum64 $checksum64 -checksumType64 $checkumType -validExitCodes $validExitCodes
