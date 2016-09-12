37a834b7d1184f3c07d9113d4dbb687af7a034f6
Hadrien Dussuel <hadrien.dussuel@gmail.com>
2016-09-12 16:31:42 +0200

    Moved packages to src dir

---
+++ src/homebank/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'homebank'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checkumType = 'sha256'
+$silentArgs = '/silent'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
---
+++ src/microsoft-r-open/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'microsoft-r-open'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checkumType = 'sha256'
+$silentArgs = '/silent'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
