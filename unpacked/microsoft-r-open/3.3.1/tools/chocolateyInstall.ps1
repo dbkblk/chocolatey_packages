6cf4ba03f127645044be0c94b238433e21f647a1
Hadrien Dussuel <dbkblk@gmail.com>
2016-11-30 22:04:39 +0100

    Update: microsoft-r-open v3.3.1.1

--- unpacked/microsoft-r-open/3.3.1/tools/chocolateyInstall.ps1
+++ unpacked/microsoft-r-open/3.3.1/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'microsoft-r-open'
 $installerType = 'MSI'
 $url = 'https://mran.revolutionanalytics.com/install/mro/3.3.1/microsoft-r-open-3.3.1.msi'
-$checksum = '0A99D2C9AA1465D25D9CB8C0DFFF07E73A13C6746A45FFD03B79C85258599747'
+$checksum = 'B366F7188D2DA214CC0D068295DF0FE2902CACE113BAEA65AF8AAF69010E3A27'
 $checkumType = 'sha256'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
