ef33338cbaf01554889d655d5c44105b88a58441
Hadrien DUSSUEL <dbkblk@users.noreply.github.com>
2017-07-04 19:32:39 +0200

    repository cleaning

--- unpacked/microsoft-r-open/3.3.1/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-$packageName = 'microsoft-r-open'
-$installerType = 'MSI'
-$url = 'https://mran.revolutionanalytics.com/install/mro/3.3.1/microsoft-r-open-3.3.1.msi'
-$checksum = 'B366F7188D2DA214CC0D068295DF0FE2902CACE113BAEA65AF8AAF69010E3A27'
-$checkumType = 'sha256'
-$silentArgs = '/quiet'
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
--- unpacked/microsoft-r-open/3.3.2/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-$packageName = 'microsoft-r-open'
-$installerType = 'MSI'
-$url = 'https://mran.revolutionanalytics.com/install/mro/3.3.2/microsoft-r-open-3.3.2.msi'
-$checksum = 'A00A6E4B5A1103ABB5B186E3EEDA8D67825601E76DAC9299BAFFD1BFDF597915'
-$checkumType = 'sha256'
-$silentArgs = '/quiet'
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
