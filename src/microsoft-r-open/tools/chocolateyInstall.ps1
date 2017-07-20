a7ed10e528ef098f442adb576276e425e21dc4fe
Hadrien DUSSUEL <dbkblk@users.noreply.github.com>
2017-07-20 15:25:46 +0200

    microsoft-r-open 3.4.0
    Revert to old command switch.
    Doubled the installation command of chocolatey and added error code 1603 as valid because the first setup sometimes fails on the automated test machine.

--- src/microsoft-r-open/tools/chocolateyInstall.ps1
+++ src/microsoft-r-open/tools/chocolateyInstall.ps1
@@ -3,6 +3,8 @@ $installerType = 'EXE'
 $url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
 $checkumType = 'sha256'
-$silentArgs = '/s'
-$validExitCodes = @(0)
+$silentArgs = '/quiet'
+$validExitCodes = @(0, 1603)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
--- unpacked/microsoft-r-open/3.4.0/tools/chocolateyInstall.ps1
+++ unpacked/microsoft-r-open/3.4.0/tools/chocolateyInstall.ps1
@@ -3,6 +3,8 @@ $installerType = 'EXE'
 $url = 'https://mran.revolutionanalytics.com/install/mro/3.4.0/microsoft-r-open-3.4.0.exe'
 $checksum = '2C203421A9779E3F68066A6AB5CBF747C435DD54EC9B8454670BBA7362B62623'
 $checkumType = 'sha256'
-$silentArgs = '/s'
-$validExitCodes = @(0)
+$silentArgs = '/quiet'
+$validExitCodes = @(0, 1603)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
