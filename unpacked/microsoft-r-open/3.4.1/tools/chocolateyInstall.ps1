7824d862d26f6201aebd28b571fa2bbd73e20c57
Hadrien DUSSUEL <dbkblk@users.noreply.github.com>
2017-07-29 17:31:15 +0200

    Update : MRO v3.4.1
    Note: As of today, the download link on the website isn't working yet.

--- unpacked/microsoft-r-open/3.4.1/tools/chocolateyInstall.ps1
+++ unpacked/microsoft-r-open/3.4.1/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = 'microsoft-r-open'
+$packageName = 'microsoft-r-open'
 $installerType = 'EXE'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$url = 'https://mran.microsoft.com/install/mro/3.4.1/microsoft-r-open-3.4.1.exe'
+$checksum = '829B543B4F664142C37453511C157ED39ADCED55216A08753A7ED4154123583D'
 $checkumType = 'sha256'
 $silentArgs = '/quiet'
 $validExitCodes = @(0, 1603)
