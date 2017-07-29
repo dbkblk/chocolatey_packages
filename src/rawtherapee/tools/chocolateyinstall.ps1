fbafd9643a442cd6d635a4d1568b2e7bbef27781
Hadrien DUSSUEL <dbkblk@users.noreply.github.com>
2017-07-29 17:30:47 +0200

    Missing Rawtherapee files

---
+++ src/rawtherapee/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'rawtherapee'
+$installerType = 'EXE'
+$url64 = '{{DownloadUrl64}}'
+$checksumZip = '{{ChecksumZip}}'
+$checksum64 = '{{Checksum}}'
+$checkumType = 'sha256'
+$silentArgs = '/verysilent'
+$validExitCodes = @(0)
+
+# Get and unzip $env:TEMP
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath "$env:TEMP\{{ZipName}}.zip" -Url64Bit "$url64" -checksum64 "$checksumZip"  -checksumType $checkumType
+Get-ChocolateyUnzip -FileFullPath "$env:TEMP\{{ZipName}}.zip" -Destination "$env:TEMP\rawtherapee\temp\"
+Install-ChocolateyPackage -PackageName $packageName -FileType "$installerType" -SilentArgs "$silentArgs" -Url64bit "$env:TEMP\rawtherapee\temp\RawTherapee_*_WinVista_64.exe" -checksum $checksum64 -checksumType $checkumType -validExitCodes $validExitCodes
+
+ # To fix
+ # Version
+ # 64 bits only
+ # Delete file
