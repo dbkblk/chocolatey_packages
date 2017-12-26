2e85d7ae8fb843cda661795681e8fee52c3b0d40
Hadrien Dussuel <dbkblk@gmail.com>
2017-12-26 13:04:42 +0100

    New package for Darktable (2.4.0)

---
+++ src/darktable/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'darktable'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checkumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -SilentArgs "$silentArgs" -Url64bit "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
