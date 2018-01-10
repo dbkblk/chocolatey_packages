671a926fb3660b645e52698bc991a44e645501ab
Hadrien Dussuel-Jarry <dbkblk@gmail.com>
2018-01-10 23:36:55 +0100

    Autopkg support for xmind (release 3.7.5)

---
+++ src/xmind/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'xmind'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checkumType = 'sha256'
+$silentArgs = '/verysilent'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
