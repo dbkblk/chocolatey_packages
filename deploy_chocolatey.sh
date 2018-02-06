237f776e6628c2e0ca8df155ad0317acc3d93d63
Hadrien Dussuel <hadrien.dussuel@blg-environnement.com>
2018-02-06 16:36:34 +0100

    t

--- deploy_chocolatey.sh
+++ deploy_chocolatey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone --depth=10 --branch=master https://github.com/chocolatey/choco.git
+git clone --depth=10 --branch=stable https://github.com/chocolatey/choco.git
 cd choco
 chmod +x build.sh
 ./build.sh
