e501055dc2f1742eac7855c35f2c555a5d818819
Hadrien Dussuel <hadrien.dussuel@blg-environnement.com>
2018-02-07 14:29:28 +0100

    darktable|2.4.1

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,10 @@
 # Get informations about the package to deploy
 $commit = (Get-ChildItem env:APPVEYOR_REPO_COMMIT_MESSAGE).Value
-$api_key = (Get-ChildItem env:CHOCO_API).Value
 $name = $commit.split('|')[0]
 $Dir = Get-ChildItem .\pkg\$name\ -recurse
 $filename = $Dir | where {$_.extension -eq ".nupkg"} | % {$_.Name}
-Write-Host $api_key
+
+# Register API key and deploy
+$api_key = (Get-ChildItem env:CHOCO_API).Value
+(choco apikey --key $api_key --source https://push.chocolatey.org/)
+(choco push -v .\pkg\$name\$filename)
