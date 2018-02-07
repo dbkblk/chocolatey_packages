dcf9fb45431cad045c644cb721051c71f5b7846f
Hadrien Dussuel <hadrien.dussuel@blg-environnement.com>
2018-02-07 11:56:10 +0100

    test

---
+++ repack.ps1
@@ -0,0 +1,30 @@
+cd .\pkg
+
+# Get package list
+$pkg = dir -Directory | Select Name
+
+# Loop packages
+$pkg | ForEach-Object {
+    $name = $_.Name
+    $v_local = Select-String -Path ".\$name\$name.nuspec" -Pattern $regex -AllMatches | % {$_.matches.groups[1]} | % {$_.Value}
+    $v_distant = (choco info -r --version=$v_local $name).split("|")[1]
+
+    # Avoid to build package if there is a problem.
+    if( [string]::IsNullOrEmpty($v_distant) )
+    {
+        Write-Error "Cannot reach pkg version for $($name). Aborting script."
+        $Host.Exit(1)
+    }
+
+    # If there is a new version, pack it.
+    If ( $v_local -ne $v_distant ) 
+    {
+        Write-Host "Packing pkg: $($name), local: $($v_local), distant: $($v_distant)"
+        cd $name
+        (choco pack)
+        cd ..
+    }
+}
+
+#echo $pkg
+cd ..
