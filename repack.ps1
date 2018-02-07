ecb1c1969eea9d6262bf62c71b5cdc8aeded392d
Hadrien Dussuel <hadrien.dussuel@blg-environnement.com>
2018-02-07 12:28:45 +0100

    xmind|76

--- repack.ps1
+++ repack.ps1
@@ -1,5 +1,24 @@
 $commit = (Get-ChildItem env:APPVEYOR_REPO_COMMIT_MESSAGE).Value
-Write-Host $commit
+$name = $commit.split('|')[0]
+$version = $commit.split('|')[1]
+Write-Host "Processing $($name) v$($version)"
+
+cd .\pkg
+
+$v_local = Select-String -Path ".\$name\$name.nuspec" -Pattern $regex -AllMatches | % {$_.matches.groups[1]} | % {$_.Value}
+$v_distant = (choco info -r --version=$v_local $name).split("|")[1]
+
+# Avoid to build package if there is a problem.
+if( [string]::IsNullOrEmpty($v_distant) )
+{
+    Write-Error "Cannot reach pkg version for $($name). Aborting script."
+    $Host.Exit(1)
+}
+
+cd $name
+(choco pack)
+cd ..
+
 # $pkg = 
 # $regex = '<version>(.*)<\/version>'
 
