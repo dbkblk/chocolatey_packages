e15b771deb7b0a3872498ddd5e75484417811e4d
Hadrien Dussuel <hadrien.dussuel@blg-environnement.com>
2018-02-07 14:09:07 +0100

    darktable|2.4.2

--- test_installation.ps1
+++ test_installation.ps1
@@ -2,5 +2,19 @@ $commit = (Get-ChildItem env:APPVEYOR_REPO_COMMIT_MESSAGE).Value
 $name = $commit.split('|')[0]
 $Dir = Get-ChildItem .\pkg\$name\ -recurse
 $filename = $Dir | where {$_.extension -eq ".nupkg"} | % {$_.Name}
+
+Write-Host "Testing installation."
 (choco install .\pkg\$name\$filename)
+if ( $LASTEXITCODE -ne 0 )
+{
+    Write-Error "An error has occured during the installation."
+    Exit(1)
+}
+
+Write-Host "Testing uninstallation."
 (choco uninstall $name)
+if ( $LASTEXITCODE -ne 0 )
+{
+    Write-Error "An error has occured during the uninstallation."
+    Exit(1)
+}
