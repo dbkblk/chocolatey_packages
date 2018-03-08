b93a5a716283e084099edd07072e7a99908c1a33
Hadrien Dussuel <dbkblk@gmail.com>
2018-03-08 16:12:23 +0100

    homebank|5.1.7

--- src/homebank/latest.ps1
+++ src/homebank/latest.ps1
@@ -1,2 +1,2 @@
 $version = '5.1.7'
-$url = 'http://homebank.free.fr/public/HomeBank-release-2.4.1/darktable-2.4.1-win64.exe-setup.exe'
+$url = 'http://homebank.free.fr/public/HomeBank-5.1.7-setup.exe'
