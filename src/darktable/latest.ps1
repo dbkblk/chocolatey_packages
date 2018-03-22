e66686a7958e3b4af90c8e6b1374e417519bf7e8
Hadrien Dussuel <dbkblk@gmail.com>
2018-03-22 18:45:11 +0100

    darktable|2.4.2

--- src/darktable/latest.ps1
+++ src/darktable/latest.ps1
@@ -1,2 +1,2 @@
-$version = '2.4.1'
-$url = 'https://github.com/darktable-org/darktable/releases/download/release-2.4.1/darktable-2.4.1-win64.exe'
+$version = '2.4.2'
+$url = 'https://github.com/darktable-org/darktable/releases/download/release-2.4.2/darktable-2.4.2-win64.exe'
