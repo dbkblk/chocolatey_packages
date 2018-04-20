402759bd5e49c64e33b1e57c351405e6ce0483b8
Hadrien Dussuel <dbkblk@gmail.com>
2018-04-20 06:37:25 +0200

    microsoft-r-open|3.4.4

--- src/microsoft-r-open/latest.ps1
+++ src/microsoft-r-open/latest.ps1
@@ -1,2 +1,2 @@
-$version = '3.4.3'
-$url = 'https://mran.blob.core.windows.net/install/mro/3.4.3/microsoft-r-open-3.4.3.exe'
+$version = '3.4.4'
+$url = 'https://mran.blob.core.windows.net/install/mro/3.4.4/microsoft-r-open-3.4.4.exe'
