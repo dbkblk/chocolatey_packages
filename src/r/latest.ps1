fcddb2442a9397b88e8034a13825142cf90f0381
Hadrien Dussuel <dbkblk@gmail.com>
2018-04-24 06:41:18 +0200

    r|3.5.0

--- src/r/latest.ps1
+++ src/r/latest.ps1
@@ -1,2 +1,2 @@
-$version = '3.4.4'
-$url = 'https://www.stats.bris.ac.uk/R/bin/windows/base/R-3.4.4-win.exe'
+$version = '3.5.0'
+$url = 'https://www.stats.bris.ac.uk/R/bin/windows/base/R-3.5.0-win.exe'
