f80e95d8fead28d4369238c645bb56bf7788609b
Hadrien Dussuel <dbkblk@gmail.com>
2018-07-03 06:48:20 +0200

    terminus|1.0.0-alpha48

--- src/terminus/latest.ps1
+++ src/terminus/latest.ps1
@@ -1,2 +1,2 @@
-$version = '1.0.0-alpha47'
-$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.0-alpha.47/terminus-Setup-1.0.0-alpha.47.exe'
+$version = '1.0.0-alpha48'
+$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.0-alpha.48/terminus-Setup-1.0.0-alpha.48.exe'
