43d08a4270b5aa47110a980f4da6799b8fbaacab
Hadrien Dussuel <dbkblk@gmail.com>
2018-07-03 06:48:29 +0200

    r|3.5.1

--- src/r/latest.ps1
+++ src/r/latest.ps1
@@ -1,2 +1,2 @@
-$version = '3.5.0'
-$url = 'https://www.stats.bris.ac.uk/R/bin/windows/base/R-3.5.0-win.exe'
+$version = '3.5.1'
+$url = 'https://www.stats.bris.ac.uk/R/bin/windows/base/R-3.5.1-win.exe'
