32f9fa9f111f222f04173fa330a0ce636c15ce23
Hadrien Dussuel <dbkblk@gmail.com>
2018-08-23 06:49:46 +0200

    terminus|1.0.0-alpha52

--- src/terminus/latest.ps1
+++ src/terminus/latest.ps1
@@ -1,2 +1,2 @@
-$version = '1.0.0-alpha51'
-$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.0-alpha.51/terminus-Setup-1.0.0-alpha.51.exe'
+$version = '1.0.0-alpha52'
+$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.0-alpha.52/terminus-Setup-1.0.0-alpha.52.exe'
