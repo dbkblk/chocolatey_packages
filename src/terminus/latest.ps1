8aee8057e3a6172fd89285411af0130d0344b872
Hadrien Dussuel <dbkblk@gmail.com>
2018-12-11 19:18:25 +0100

    terminus|1.0.65

--- src/terminus/latest.ps1
+++ src/terminus/latest.ps1
@@ -1,2 +1,2 @@
-$version = '1.0.1'
-$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.1/terminus-Setup-1.0.1.exe'
+$version = '1.0.65'
+$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.65/terminus-Setup-1.0.65.exe'
