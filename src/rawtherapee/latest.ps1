0f7af28842ac61a8121b10bd33bc3e4b8d2a3bdd
Hadrien Dussuel <dbkblk@gmail.com>
2018-12-17 18:42:54 +0100

    rawtherapee|5.5

--- src/rawtherapee/latest.ps1
+++ src/rawtherapee/latest.ps1
@@ -1,2 +1,2 @@
-$version = '5.4'
-$url = 'http://rawtherapee.com/releases_head/windows/RawTherapee_5.4_WinVista_64.zip'
+$version = '5.5'
+$url = 'http://rawtherapee.com/releases_head/windows/RawTherapee_5.5_WinVista_64.zip'
