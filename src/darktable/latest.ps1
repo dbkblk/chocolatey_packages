f233afee489f323242e0313605217a2f103be3e4
Hadrien Dussuel <dbkblk@gmail.com>
2019-03-08 19:10:10 +0100

    darktable|2.6.1

--- src/darktable/latest.ps1
+++ src/darktable/latest.ps1
@@ -1,2 +1,2 @@
-$version = '2.6.0'
-$url = 'https://github.com/darktable-org/darktable/releases/download/release-2.6.0/darktable-2.6.0-win64.exe'
+$version = '2.6.1'
+$url = 'https://github.com/darktable-org/darktable/releases/download/release-2.6.1/darktable-2.6.1-win64.exe'
