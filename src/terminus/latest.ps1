de815c99c874d70157f5ba4a6f2a1b9fc4e18460
Hadrien Dussuel <dbkblk@gmail.com>
2019-03-10 10:55:03 +0100

    terminus|1.0.72

--- src/terminus/latest.ps1
+++ src/terminus/latest.ps1
@@ -1,2 +1,2 @@
-$version = '1.0.71'
-$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.71/terminus-Setup-1.0.71.exe'
+$version = '1.0.72'
+$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.72/terminus-Setup-1.0.72.exe'
