970a52c3b3e8596e981de6e5346491ee6a9f7eb8
Hadrien Dussuel <dbkblk@gmail.com>
2019-05-16 20:45:39 +0200

    microsoft-r-open|3.5.2
    Oops, wrong update link.

--- src/microsoft-r-open/latest.ps1
+++ src/microsoft-r-open/latest.ps1
@@ -1,2 +1,2 @@
 $version = '3.5.2'
-$url = 'https://mran.blob.core.windows.net/install/mro/3.5.2/microsoft-r-open-3.5.2.exe'
+$url = 'https://mran.blob.core.windows.net/install/mro/3.5.2/windows/microsoft-r-open-3.5.2.exe'
