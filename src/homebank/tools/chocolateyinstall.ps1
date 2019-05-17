a1f04b2c8a1658b4d2ad4a0c2a9b718b990f277c
Hadrien DUSSUEL <dbkblk@gmail.com>
2019-05-17 12:55:55 +0200

    homebank|5.2.5
    Fixed a new switch which was introduced in the 5.2.4 installer (probably an update?)

--- src/homebank/tools/chocolateyinstall.ps1
+++ src/homebank/tools/chocolateyinstall.ps1
@@ -3,6 +3,6 @@ $installerType = 'EXE'
 $url = '{{url}}'
 $checksum = '{{checksum}}'
 $checkumType = 'sha256'
-$silentArgs = '/verysilent'
+$silentArgs = '/verysilent /allusers'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
