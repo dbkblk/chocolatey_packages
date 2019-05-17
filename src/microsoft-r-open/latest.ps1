41f490dde88b4fcad7cd3ea8229bd62ec8deb324
Hadrien Dussuel <dbkblk@gmail.com>
2019-05-17 18:12:30 +0200

    microsoft-r-open|3.5.3

--- src/microsoft-r-open/latest.ps1
+++ src/microsoft-r-open/latest.ps1
@@ -1,2 +1,2 @@
-$version = '3.5.2'
-$url = 'https://mran.blob.core.windows.net/install/mro/3.5.2/windows/microsoft-r-open-3.5.2.exe'
+$version = '3.5.3'
+$url = 'https://mran.blob.core.windows.net/install/mro/3.5.3/windows/microsoft-r-open-3.5.3.exe'
