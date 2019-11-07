ee17ce0a3e13fd60c0bab415eb2ac5cd3a0a6246
Hadrien Dussuel <hadrien.dussuel@posteo.net>
2019-11-07 16:17:20 +0100

    "terminus|1.0.92"

--- src/terminus/latest.ps1
+++ src/terminus/latest.ps1
@@ -1,2 +1,2 @@
-$version = '1.0.79'
-$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.79/terminus-1.0.79-setup.exe'
+$version = '1.0.92'
+$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.92/terminus-1.0.92-setup.exe'
