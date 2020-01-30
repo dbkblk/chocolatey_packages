7cbd5bf69de4aa40e0214870aa23b1cc4892ae47
Hadrien Dussuel <dbkblk@gmail.com>
2020-01-30 18:00:07 +0100

    "terminus|1.0.101"

--- src/terminus/latest.ps1
+++ src/terminus/latest.ps1
@@ -1,2 +1,2 @@
-$version = '1.0.100'
-$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.100/terminus-1.0.100-setup.exe'
+$version = '1.0.101'
+$url = 'https://github.com/Eugeny/terminus/releases/download/v1.0.101/terminus-1.0.101-setup.exe'
